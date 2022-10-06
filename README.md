391b04ba448c6b154c5b46b7bab18120301c9e45
jumpyapple <70656949+jumpyapple@users.noreply.github.com>
2022-10-06 03:56:33 -0400

    chore: add draft of the entrire project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ajax-image-django-app
+
+## Specification
+
+Make one of these work
+
+- https://stackoverflow.com/questions/62049649/capture-web-camera-image-and-upload-database-using-django
+- https://stackoverflow.com/questions/68130675/how-can-i-capture-a-picture-from-webcam-and-store-it-in-a-imagefield-or-filefiel
+- https://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata
---
+++ img_app/__init__.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        print(request)
+        return jsonify({"status": "successful"})
+
+
+if __name__ == "__main__":
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
